f2864c0b0948009069460488533e55e3b992cc4d
Suraj Singh <yousurajsingh@gmail.com>
2025-01-30 22:44:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
-//your JS code here. If required.
+
+
+document.getElementById("square1").addEventListener("mouseover", ()=>{
+
+    document.getElementById("square1").style.backgroundColor = "#E6E6FA";
+    document.getElementById("square2").style.backgroundColor = "#6F4E37";
+    document.getElementById("square3").style.backgroundColor = "#6F4E37";
+
+});
+
+document.getElementById("square2").addEventListener("mouseover", ()=>{
+
+    document.getElementById("square2").style.backgroundColor = "#E6E6FA";
+    document.getElementById("square1").style.backgroundColor = "#6F4E37";
+    document.getElementById("square3").style.backgroundColor = "#6F4E37";
+
+});
+  
+
+document.getElementById("square3").addEventListener("mouseover", ()=>{
+
+    document.getElementById("square3").style.backgroundColor = "#E6E6FA";
+    document.getElementById("square1").style.backgroundColor = "#6F4E37";
+    document.getElementById("square2").style.backgroundColor = "#6F4E37";
+
+});
+
+document.getElementById("square1").addEventListener("mouseleave", ()=>{
+
+    document.getElementById("square3").style.backgroundColor = "#E6E6FA";
+    document.getElementById("square1").style.backgroundColor = "#E6E6FA";
+    document.getElementById("square2").style.backgroundColor = "#E6E6FA";
+
+});
+document.getElementById("square2").addEventListener("mouseleave", ()=>{
+
+    document.getElementById("square3").style.backgroundColor = "#E6E6FA";
+    document.getElementById("square1").style.backgroundColor = "#E6E6FA";
+    document.getElementById("square2").style.backgroundColor = "#E6E6FA";
+
+});
+document.getElementById("square3").addEventListener("mouseleave", ()=>{
+
+    document.getElementById("square3").style.backgroundColor = "#E6E6FA";
+    document.getElementById("square1").style.backgroundColor = "#E6E6FA";
+    document.getElementById("square2").style.backgroundColor = "#E6E6FA";
+
+});
+
+  
